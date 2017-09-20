4835cce8abd95b9a726f11dada364562150581dc
Alvin Niamatali and Daniel Cheng <daniel@>
2017-09-20 16:28:45 -0700

    Javascript translation working

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,16 @@ function vowelCheck(letter) {
   }
 }
 
+function consonantCheck(letter) {
+  var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U", "qu", "Qu", "qU", "QU"];
+  var consonantFound = false;
+  for (var i=0; i <= vowels.length; i++) {
+    if (letter !== vowels[i]) {
+      var consonantFound = true;
+    }
+  }
+}
+
 
 $(document).ready(function() {
    $("#formOne").submit(function(event) {
@@ -25,13 +35,25 @@ $(document).ready(function() {
      });
 
      parentArray.forEach(function(babyArray) {
-        var firstLetter = babyArray.indexOf(1);
-         if (vowelCheck(firstLetter) ===  true);
-          finalArray.push(babyArray.join("") +"way");
 
+      // if first letter is vowel add "way"
+      var firstLetter = babyArray.indexOf(1);
+      if (vowelCheck(firstLetter) ===  true) {
+      }
+        finalArray.push(babyArray.join("") +"way");
+// debugger;
+      // move consonants to back and add "ay"
+      // babyArray.split("");
+      for (var i=0; i <= babyArray.length; i++) {
+        if (consonantCheck(babyArray[i]) === true) {
+          var consonantWord = babyArray.substr(1)+babyArray.split("",1);
+        }
+      }
+      finalArray.push(consonantWord+"way");
+
+      console.log(consonantWord);
      });
-     console.log(finalArray)
-;
+     console.log(finalArray);
 
 
      event.preventDefault();
---
+++ js/scripts2.js
@@ -0,0 +1,79 @@
+function vowelCheck(letter) {
+  var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  var vowelFound = false;
+  for (var i=0; i <= vowels.length; i++) {
+    if (letter === vowels[i]) {
+      var vowelFound = true;
+    }
+  }
+  return vowelFound;
+}
+
+function consonantCheck(letter) {
+  var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  var consonantFound = false;
+  for (var i=0; i <= vowels.length; i++) {
+    if (vowelCheck(letter) === false && letter !== "q" && letter !== "Q") {
+      var consonantFound = true;
+    }
+  }
+  return consonantFound;
+}
+
+function quCheck(letter) {
+  var qu = ["q", "Q"];
+  var quFound = false;
+  for (var i=0; i <= qu.length; i++) {
+    if (letter == qu[i]) {
+      var quFound = true;
+    }
+  }
+  return quFound;
+}
+
+$(document).ready(function() {
+   $("#formOne").submit(function(event) {
+
+     var sentence = $("#sentence").val().split(" ");
+     var parentArray = [];
+     var finalArray = [];
+    //  var letters = [];
+    //  console.log(sentence);
+
+//parentArray = [babyArray babyArray babyArray...babyArray]
+    //  sentence.forEach(function(word) {
+    //    var letters = word.split("");
+    //    parentArray.push(letters);
+    //  });
+    // debugger;
+     sentence.forEach(function(word) {
+       var letter = word.charAt(0);
+      if (vowelCheck(letter) === true) {
+        finalArray.push(word + "way");
+      } else if (consonantCheck(letter) === true) {
+        debugger;
+        for (var i=0; i < word.length; i++) {
+          if (consonantCheck(letter) === true) {
+            word = word.substr(1)+ word.split("",1);
+            letter = word.charAt(0);
+          }
+        }
+        for (var i=0; i < word.length; i++) {
+          if (quCheck(letter) === true) {
+            word = word.substr(2)+"qu";
+            letter = word.charAt(0);
+          }
+        }
+        finalArray.push(word +"ay");
+
+      } else if (quCheck(letter) === true) {
+        var quWord = word.substr(2)+"qu";
+        finalArray.push(quWord +"ay");
+      }
+        alert(finalArray);
+    });
+
+     event.preventDefault();
+
+   });
+});
