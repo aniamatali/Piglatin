1dbfbaa3fafc4a5a62c25ea689608a0fd818849f
Alvin Niamatali and Daniel Cheng <daniel@>
2017-09-20 10:57:48 -0700

    way is added to each string after the loop runs into a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,37 @@
+function vowelCheck(letter) {
+  var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  var vowelFound = false;
+  for (var i=0; i <= vowels.length; i++) {
+    if (letter === vowels[i]) {
+      var vowelFound = true;
+    }
+  }
+}
+
+
 $(document).ready(function() {
    $("#formOne").submit(function(event) {
 
      var sentence = $("#sentence").val().split(" ");
-     var splitwords = [];
+     var parentArray = [];
+     var finalArray = [];
     //  var letters = [];
     //  console.log(sentence);
 
+//parentArray = [babyArray babyArray babyArray...babyArray]
      sentence.forEach(function(word) {
        var letters = word.split("");
-       splitwords.push(letters);
+       parentArray.push(letters);
      });
-     console.log(splitwords);
 
+     parentArray.forEach(function(babyArray) {
+        var firstLetter = babyArray.indexOf(1);
+         if (vowelCheck(firstLetter) ===  true);
+          finalArray.push(babyArray.join("") +"way");
 
+     });
+     console.log(finalArray)
+;
 
 
      event.preventDefault();
